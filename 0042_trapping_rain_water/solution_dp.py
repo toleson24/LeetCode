39a50408e57bc2f714c6775c60112ddfecfba891
againTraviscodes <t.uni.versity17@gmail.com>
2025-05-13 21:11:00 -0700

    updated dp solution using more efficient sum variable

--- 0042_trapping_rain_water/solution_dp.py
+++ 0042_trapping_rain_water/solution_dp.py
@@ -1,13 +1,13 @@
 from typing import List
 
-# runtime: 36.23%, memory: 12.52%
+# runtime: 37.68%, memory: 40.06%
 class Solution:
     def trap(self, height: List[int]) -> int:
+        water: int = 0
         length: int = len(height)
         
         l_max: List[int] = [0] * length
         r_max: List[int] = [0] * length
-        water: List[int] = [0] * length
 
         _max: int = 0
         for i in range(length - 1):
@@ -22,8 +22,10 @@ class Solution:
             r_max[i] = _max
         
         for i in range(length - 1):
-            water[i] = max(min(l_max[i], r_max[i]) - height[i], 0)
+            water += max(min(l_max[i], r_max[i]) - height[i], 0)
 
-        return sum(water)
+        return water
+
+        
 
         
