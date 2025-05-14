1506c4ef7d7e26e4117ebf42e3eded70b80ea88d
againTraviscodes <t.uni.versity17@gmail.com>
2025-05-13 22:22:13 -0700

    added LeetCode 143 Reorder List in Java

---
+++ 0143_reorder_list/Solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// runtime: 86.42%, memory: 15.60%
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null)
+            return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reverse second half of the list
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode node = null;
+
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = node;
+            node = second;
+            second = temp;
+        }
+
+        // merge lists
+        ListNode first = head;
+        second = node;
+
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
