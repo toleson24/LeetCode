2216d81a1f0f3c9f012e31d70d0e05967208efc1
againTraviscodes <t.uni.versity17@gmail.com>
2025-12-15 21:45:20 -0800

    added LeetCode 155 Min Stack in Python using a two stack approach

---
+++ 0155_min_stack/solution_two_stack.py
@@ -0,0 +1,37 @@
+from typing import List 
+
+# runtime: 100.00%, memory: 60.66% 
+class MinStack:
+
+    def __init__(self):
+        self.stack: List[int] = []
+        self.min_stack: List[int] = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        
+
+    def pop(self) -> None:
+        val: int = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
