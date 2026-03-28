d7b09303067d68d92e8b241e2433966bfc03937c
againTraviscodes <t.uni.versity17@gmail.com>
2026-03-28 10:03:47 -0700

    added LeetCode 141 Linked List Cycle in Java using the two pointer (fast/slow) technique

---
+++ 0141_linked_list_cycle/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+// runtime: 100.00%, memory: 25.93% 
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
